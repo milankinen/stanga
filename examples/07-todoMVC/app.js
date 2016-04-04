4f7b02e0fc91028caa77c0a42be3c0be9bb0c5ac
Géraud Henrion <geraud.henrion@gmail.com>
2016-04-04 22:55:18 +0200

    removes localstorage driver for a leaner example

--- examples/07-todoMVC/app.js
+++ examples/07-todoMVC/app.js
@@ -1,11 +1,7 @@
 import { run } from "@cycle/core"
 import { Observable as O } from "rx"
 import { makeDOMDriver, footer, div, p, section } from "@cycle/dom"
-import storageDriver from "@cycle/storage"
 import { Model } from "stanga"
-
-import deserialize from "./storage-source"
-import serialize from "./storage-sink"
 
 import { changeFilter } from "./actions"
 import Todos from "./components/Todos"
@@ -20,34 +16,20 @@ run(main, {
   DOM: makeDOMDriver("#app"),
   hash: () => O.just("")
     .concat(O.fromEvent(window, "hashchange")) // eslint-disable-line no-undef
-    .map(() => window.location.hash.replace("#", "")), // eslint-disable-line no-undef
-  storage: storageDriver
+    .map(() => window.location.hash.replace("#", "")) // eslint-disable-line no-undef
 })
 
-function main({DOM, M, hash, storage}) {
-  const initialState$ = deserialize(storage.local
-    .getItem("todos-cycle")
-    .take(1)
-    .shareReplay(1)
-  )
-  const storageSink$ = serialize(M)
-    .map((state) => ({
-      key: "todos-cycle",
-      value: state
-    }))
+function main({DOM, M, hash}) {
 
 
   const todos = Todos({DOM, M})
   const mod$ = O.merge(
-    M.set(initialState$),
     M.mod(hash.map(changeFilter)),
     todos.M
   )
 
   return {
     M: mod$,
-    storage: storageSink$,
-
     DOM: O.of(div([
       section(".todoapp", todos.DOM),
       footer(".info", [
